1ee93a844e2c44e646ade2c6369d1c85d82a89ed
fjar86github <84884351+fjar86github@users.noreply.github.com>
2024-12-15 18:49:40 -0700

    Delete frontend directory

--- frontend/app.js
+++
@@ -1,7 +0,0 @@
-fetch('/api/read.php')
-    .then(response => response.json())
-    .then(data => {
-        const container = document.getElementById('data');
-        container.innerHTML = JSON.stringify(data);
-    })
-    .catch(err => console.error('Error:', err));
