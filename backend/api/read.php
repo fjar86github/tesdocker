80bbb95ab32db4df170614795c7910631dc2c618
fjar86github <84884351+fjar86github@users.noreply.github.com>
2024-12-15 18:49:29 -0700

    Delete backend directory

--- backend/api/read.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$pdo = new PDO('mysql:host=db;dbname=testdb', 'root', 'rootpassword');
-$stmt = $pdo->query('SELECT * FROM items');
-$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($data);
-?>
