d3b1aefcb0f2de1d49d0119dedca28b8978f524c
fjar86github <84884351+fjar86github@users.noreply.github.com>
2024-12-15 19:01:16 -0700

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -1,1 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'mydb';
+$username = 'root';
+$password = 'root';
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
